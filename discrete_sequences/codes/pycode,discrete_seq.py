4d835c1c0d0d7097922123262f75f137740e1e7f
psrikarvarma <155625152+psrikarvarma@users.noreply.github.com>
2024-02-04 17:26:33 +0530

    Update pycode,discrete_seq.py

--- discrete_sequences/codes/pycode,discrete_seq.py
+++ discrete_sequences/codes/pycode,discrete_seq.py
@@ -2,51 +2,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Function definitions
-def func1(x):
-    return 2**(x + 3)
+def func1(n):
+    return np.where((2 < n) & (n < 5), 2**(n + 3), 0)
 
-def func2(x):
-    return 2**(5 - x)
+def func2(n):
+    return np.where((2 < n) & (n < 5), 2**(5 - n), 0)
 
-def func3(x):
-    return 8 / (1 - 2 * (1 / x))
+def func3(z):
+    return np.where(abs(z) > 2, 8 / (1 - 2 * z**(-1)), 0)
 
-def func4(x):
-    return 32 / (1 - (2 * x)**(-1))
+def func4(z):
+    return np.where(abs(z) > 2, 32 / (1 - 1/(2*z)), 0)
 
-# Generate values for x
-x_values = np.linspace(0.1, 5, 100)
+# Generate values for n and z
+n_values = np.arange(2.1, 5, 0.1)
+z_values = np.arange(2.1, 5, 0.1)
 
-# Plotting
+# Calculate corresponding y values for each function
+y_values1 = func1(n_values)
+y_values2 = func2(n_values)
+y_values3 = func3(z_values)
+y_values4 = func4(z_values)
+
+# Plotting the stem graphs
 plt.figure(figsize=(12, 8))
 
 plt.subplot(2, 2, 1)
-plt.stem(x_values, func1(x_values), label=r'$2^{x+3}$')
-plt.title('Graph of $2^{x+3}$')
-plt.xlabel('x')
-plt.ylabel('Function Value')
-plt.legend()
+plt.stem(n_values, y_values1, basefmt='b', linefmt='r-', markerfmt='ro')
+plt.title('$2^{n+3}$')
+plt.xlabel('$n$')
+plt.ylabel('Amplitude')
 
 plt.subplot(2, 2, 2)
-plt.stem(x_values, func2(x_values), label=r'$2^{5-x}$')
-plt.title('Graph of $2^{5-x}$')
-plt.xlabel('x')
-plt.ylabel('Function Value')
-plt.legend()
+plt.stem(n_values, y_values2, basefmt='b', linefmt='r-', markerfmt='ro')
+plt.title('$2^{5-n}$')
+plt.xlabel('$n$')
+plt.ylabel('Amplitude')
 
 plt.subplot(2, 2, 3)
-plt.stem(x_values, func3(x_values), label=r'$\frac{8}{1-2(x^{-1})}$')
-plt.title('Graph of $\\frac{8}{1-2(x^{-1})}$')
-plt.xlabel('x')
-plt.ylabel('Function Value')
-plt.legend()
+plt.stem(z_values, y_values3, basefmt='b', linefmt='r-', markerfmt='ro')
+plt.title('$\\frac{8}{1-2z^{-1}}$')
+plt.xlabel('$z$')
+plt.ylabel('Amplitude')
 
 plt.subplot(2, 2, 4)
-plt.stem(x_values, func4(x_values), label=r'$\frac{32}{1-(2x)^{-1}}$')
-plt.title('Graph of $\\frac{32}{1-(2x)^{-1}}$')
-plt.xlabel('x')
-plt.ylabel('Function Value')
-plt.legend()
+plt.stem(z_values, y_values4, basefmt='b', linefmt='r-', markerfmt='ro')
+plt.title('$\\frac{32}{1-\\frac{1}{2z}}$')
+plt.xlabel('$z$')
+plt.ylabel('Amplitude')
 
 plt.tight_layout()
 plt.show()
